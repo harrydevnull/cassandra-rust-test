fb93126b136a645beea7e4be9491663c18bef7cf
harrydevnull <hareendran.c.nair@gmail.com>
2016-12-14 00:28:15 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+extern crate cdrs;
+
+use cdrs::client::CDRS;
+use cdrs::authenticators::PasswordAuthenticator;
+use cdrs::compression;
+fn main() {
+    println!("Hello, world!");
+    let user = "dart".to_string();
+    let pass = "dart".to_string();
+    let authenticator = PasswordAuthenticator::new(user, pass);
+    let addr = "localhost".to_string();
+    // pass authenticator into CDRS' constructor
+    let mut client = CDRS::new(addr, authenticator).unwrap();
+
+    // without compression
+    let response_frame = client.start(compression::Compression::Snappy).unwrap();
+    // let select_query = String::from("SELECT * FROM ks.table;");
+
+    // match client.query(select_query) {
+    //     Ok(res) => {
+    //         println!("Result frame: {:?},\nparsed body: {:?}",
+    //                  res,
+    //                  res.get_body())
+    //     }
+    //     Err(err) => println!("Error: {:?}", err),
+    // }
+
+
+}
